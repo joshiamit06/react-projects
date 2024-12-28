a8fdb2d3d3f8df5ceab95ea21c906ebbb6fb17d5
Amit Joshi <amit.joshi314@gmail.com>
2024-12-28 17:10:16 +0530

    Add useEffect and loading spinner

--- social-media-with-dummy-api/src/components/PostList.jsx
+++ social-media-with-dummy-api/src/components/PostList.jsx
@@ -1,21 +1,46 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SocialMediaContext } from "../store/social-media-store";
 import { MdDelete } from "react-icons/md";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { addPostsInBulk, posts, deletePost } = useContext(SocialMediaContext);
+  const [fetching, setFetching] = useState(false)
 
-  const handleDummyData = () => {
-    fetch("https://dummyjson.com/posts")
-      .then((res) => res.json())
-      .then((data) => addPostsInBulk(data.posts));
-  };
+  useEffect(() => {
+    setFetching(true);
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch("https://dummyjson.com/posts", { signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch posts");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        addPostsInBulk(data.posts);
+        setFetching(false);
+      })
+      .catch((error) => {
+        if (error.name !== "AbortError") {
+          console.error("Fetch error:", error.message);
+        }
+      })
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
 
   return (
     <div className="post-list-container" key={posts.id}>
-      {posts.length === 0 ? (
-        <WelcomeMessage handleDummyData={handleDummyData} />
+      {fetching && <LoadingSpinner/>}
+      {!fetching && (posts.length === 0 ? (
+        <WelcomeMessage/>
       ) : (
         posts.map((post) => (
           <div className="card post-card" style={{ width: "25rem" }}>
@@ -45,7 +70,7 @@ const PostList = () => {
             </div>
           </div>
         ))
-      )}
+      ))}
     </div>
   );
 };
--- social-media-with-dummy-api/src/components/WelcomeMessage.jsx
+++ social-media-with-dummy-api/src/components/WelcomeMessage.jsx
@@ -2,7 +2,7 @@ const WelcomeMessage = ({handleDummyData}) => {
   return (
     <div className="empty-message">
       <p>No posts to display. Create a new post!</p>
-      <button className="fetch-dummy-data-btn" onClick={handleDummyData}>
+      <button className="fetch-dummy-data-btn">
         Fetch Dummy Data From Server
       </button>
     </div>
